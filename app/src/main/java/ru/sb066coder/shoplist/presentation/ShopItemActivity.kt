7a1ba621a7bfc3c7a5758f5adcbc5e837217f2d6
sergey <sergey066@yandex.ru>
2023-04-05 20:07:35 +0300

    2nd screen layout created

---
+++ app/src/main/java/ru/sb066coder/shoplist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package ru.sb066coder.shoplist.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.sb066coder.shoplist.R
+
+class ShopItemActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
